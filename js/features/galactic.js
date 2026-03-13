82a3777bc7a9b3fada981f6f1f841d19814b30d7
Dezje333 <Dezje@me.com>
2026-03-13 17:04:35 +0100

    Update galactic.js

--- js/features/galactic.js
+++ js/features/galactic.js
@@ -168,8 +168,8 @@ function getAPEff(id) { return Decimal.pow(AP_BONUS_BASE[id],player.astralPresti
 UPGS.moonstone = {
     title: "Moonstone Upgrades",
 
-    autoUnl: ()=>hasSolarUpgrade(0,13),
-    noSpend: ()=>hasSolarUpgrade(0,13),
+    autoUnl: ()=>hasUpgrade('auto',3),
+    noSpend: ()=>hasUpgrade('auto',6),
 
     underDesc: ()=>`You have ${format(player.moonstone,0)} Moonstone (${formatPercent(tmp.moonstoneChance)} platinum grow chance)`,
 
