6ddab5fe1d51a4794de82eab8a1d6f01a079c4bc
Dezje333 <Dezje@me.com>
2026-03-13 17:05:58 +0100

    Update sciencelab.js

--- js/features/sciencelab.js
+++ js/features/sciencelab.js
@@ -1,12 +1,1 @@
-UPGS.sciencelab = {
-    unl: ()=>(player.bestGJ>=30||player.fTimes>0)&&player.unnatural,
 
-    title: "Tube Upgrades",
-
-    autoUnl: ()=>hasSolarUpgrade(0,13),
-    noSpend: ()=>hasSolarUpgrade(0,13),
-
-    req: ()=>player.grassjump>=30,
-    reqDesc: ()=>`Reach 30 Grass-Jump to Unlock.`,
-
-    underDesc: ()=>`You have ${format(player.stardust,0)} Stardust`+gainHTML(player.stardust,tmp.stardustGain,1),
